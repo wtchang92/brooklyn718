53d833335f4c2f803f7a6c44b89715e109a7678e
wtchang92 <wtchang051592@gmail.com>
2015-07-11 17:02:13 -0400

    ready for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150711055725) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
     t.string   "email"
     t.datetime "created_at", null: false
